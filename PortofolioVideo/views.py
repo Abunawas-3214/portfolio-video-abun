e5ea894617a49085d8fbb2e1b4e3208a80ddf89e
Abunawas <abunawas.arifandi@gmail.com>
2022-10-25 23:10:43 +0700

    recode views.py

--- PortofolioVideo/views.py
+++ PortofolioVideo/views.py
@@ -1,17 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
-
+    return render(request, 'index.html', context)
 
 def resume(request):
     return render(request, 'resume.html')
-
-
-def portfolio(request):
-    return render(request, 'index.html')
-
 
 def contact(request):
     if request.method == 'POST':
